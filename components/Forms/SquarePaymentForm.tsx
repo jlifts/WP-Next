938377f388e78a6363ace27fe0df790503e1e341
Josh Burgess <burgessj247@gmail.com>
2021-10-14 20:11:28 -0400

    MVP

--- components/Forms/SquarePaymentForm.tsx
+++ components/Forms/SquarePaymentForm.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable react/jsx-one-expression-per-line */
